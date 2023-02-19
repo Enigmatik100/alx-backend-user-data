ca71ae2c13249cd8d8f519c1d00efe7eacefa011
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-19 19:16:07 +0100

    decode base64 header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 Module to handle user's authentification
 """
+import base64
+
 from .auth import Auth
 
 
@@ -18,3 +20,14 @@ class BasicAuth(Auth):
         auth_details = authorization_header.split()
         if len(auth_details) > 1:
             return auth_details[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) != str:
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception as e:
+            return None
