d3bd3756d73b557461c578bd21e103923860e4c4
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-19 19:50:43 +0100

    extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,7 +35,10 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+    ) -> (str, str):
         """extract user's credentials"""
         if decoded_base64_authorization_header is None or \
                 type(decoded_base64_authorization_header) != str or \
