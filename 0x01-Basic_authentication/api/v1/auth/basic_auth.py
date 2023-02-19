89462be5676668273ec74a2093b3f17abef8cca4
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-19 20:50:20 +0100

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,16 +54,21 @@ class BasicAuth(Auth):
             user_email: str,
             user_pwd: str) -> TypeVar('User'):
         """get current user's credentials """
-        if user_email is None or user_pwd is None:
-            return None
         if type(user_email) == str and type(user_pwd) == str:
             obj = {'email': user_email}
-            if not User.all():
-                return None
-            user = User.search(obj)
-            if user:
-                if user[0].is_valid_password(user_pwd):
-                    return user[0]
+            users = User.search(obj)
+            if users:
+                if users[0].is_valid_password(user_pwd):
+                    return users[0]
                 return None
             return None
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
