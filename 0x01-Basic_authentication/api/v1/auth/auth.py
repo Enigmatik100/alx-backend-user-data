01116706c828d9aa065ffcaf648fd8885762ec76
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-19 17:28:28 +0100

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@
 Module to handle user's authentification
 """
 from typing import List, TypeVar
-
+import re
 from flask import request
 
 
@@ -11,17 +11,27 @@ class Auth:
     """Class to handle authentification"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require authentication"""
-        if path is None:
-            return True
-        elif not excluded_paths:
-            return True
-        path = path + '/' if path != '' and path[-1] != '/' else path
-        return path not in excluded_paths
+        """ returns True if the path is not in \
+            the list of strings excluded_paths """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """intercept authorization header"""
-        return None
+        if request is None:
+            return None
+        authorization = request.headers.get('Authorization', None)
+        return authorization
 
     def current_user(self, request=None) -> TypeVar('User'):
         """get the current user"""
