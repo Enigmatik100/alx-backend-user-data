fe91a1ed3a2558552766f975591c3c905e8c9a7c
enigmatik100 <m.p.gangbadja@gmail.com>
2023-02-19 13:05:38 +0100

    unauthorized display error fixed

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['get'] , strict_slashes=False)
-def unauthorized():
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     """
     abort(401)
